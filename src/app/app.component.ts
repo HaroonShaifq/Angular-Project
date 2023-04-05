4545be884866148c337aba1498a81f894ba76c4c
Haroon Shafiq <Haroonshafiq090@gmail.com>
2023-04-05 08:16:24 +0500

    Initial commit for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'first_app';
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+  postId;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.post<Article>('https://reqres.in/api/posts', { name: 'Haroon' }).subscribe(data => {
+        this.postId = data.name;
+    })
+}
+}
+
+interface Article {
+  id: number;
+  name: string;
 }
